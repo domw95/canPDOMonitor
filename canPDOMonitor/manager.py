790b58e7b32614675a295d157ef1982aab596d1d
Dom <dw470@bath.ac.uk>
2020-09-24 18:05:35 +0100

    Added Datalogger

---
+++ canPDOMonitor/manager.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 12:52:32 2020
+
+@author: Research
+"""
+
+class Manager:
+    """
+    Main class for managing PDO conversion, logging, gui etc
+    
+    contains a PDO converter object to bring in datapoints from can bus
+    routes these to any logger or graphics that are active
+    """
+    __init__(self):
+        pass
